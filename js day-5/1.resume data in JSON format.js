ab09a056e569df83dbce60f3b27b058086c15e75
ARUNKUMARP123 <154363796+ARUNKUMARP123@users.noreply.github.com>
2024-01-05 18:33:48 +0530

    Add files via upload

---
+++ js day-5/1.resume data in JSON format.js
@@ -0,0 +1,10 @@
+//1.resume data in JSON format
+var resume=[{
+    "Name":"Arunkumar.P",
+    "FatherName":"pachai",
+    "Age":25,
+    "Gender":"male",
+    "Qualification":"BE-EEE",
+    "Percentage":61.5
+}]
+//console.log(resume[0].FatherName);
---
+++ js day-5/2.iterate over all for loops.js
@@ -0,0 +1,20 @@
+//2.resume data in JSON format and iterate over all for loops (for,for in,for of,forEach).
+var resume=[{
+    "Name":"Arunkumar.P",
+    "FatherName":"pachai",
+    "Age":25,
+    "Gender":"male",
+    "Qualification":"BE-EEE",
+    "Percentage":61.5
+}]
+//for loop:
+for(let i=0;i<resume.length;i++)
+{
+    console.log(resume[i]);
+}
+
+//for in loop:
+for(var r in resume)
+{
+    console.log(r,resume);
+}
